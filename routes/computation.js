d7c989efd15eee55574c3809d8572f41dd112088
Mupparaju19 <143019131+Mupparaju19@users.noreply.github.com>
2023-10-25 15:43:58 -0500

    added computation

---
+++ routes/computation.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var m;
+var k;
+var imul;
+var log;
+var log10;
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    router.put( k = Math.random() * 10);
+    router.put( m = Math.round(k));
+    router.post(imul = Math.imul(m));
+    router.post(log = Math.log(m));
+    router.post(log10 = Math.log10(m));
+    
+    res.send(`imul applied to ${m} is ${imul} log applied to ${m} is ${log} log10 applied to ${m} is ${log10}`);
+  
+});
+
+module.exports = router;
